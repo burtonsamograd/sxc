281faea50acd98ea194e12e9475d0700ecfffbb9
Burton Samograd <burton.samograd@gmail.com>
2011-09-11 11:11:06 -0600

    scripts: add -Wall to gcc compile command

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 for i in $(find tests -name '*.sxc' | sort -n); do
     echo -n $i...
     { ./sxc.sh $i > x.c &&
-	gcc x.c &&
+	gcc -Wall x.c &&
 	{ [ -f $i.in ] &&
 	    { ./a.out < $i.in | cmp $i.out - ; } ||
 	    { ./a.out | cmp $i.out - ; }
